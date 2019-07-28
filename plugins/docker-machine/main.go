a94bf6045e0d9d73564078b7769943d39c2b95b6
Ole Claussen <claussen.ole@gmail.com>
2019-07-28 18:51:03 +0200

    added docker-machine plugin

---
+++ plugins/docker-machine/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/hashicorp/go-plugin"
+	"github.com/oclaussen/dodo/pkg/stage"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.SetFormatter(new(log.JSONFormatter))
+	plugin.Serve(&plugin.ServeConfig{
+		GRPCServer:      plugin.DefaultGRPCServer,
+		HandshakeConfig: stage.HandshakeConfig("docker-machine"),
+		Plugins: map[string]plugin.Plugin{
+			"stage": &stage.Plugin{Impl: &Stage{}},
+		},
+	})
+}
